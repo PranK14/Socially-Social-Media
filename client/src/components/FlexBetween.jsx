836fd9269cb70b47fd2ba68f52a023109a720efe
unknown <priyank.sinha14@gmail.com>
2023-07-16 22:42:47 +0530

    client folder

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+const FlexBetween = styled(Box) ({
+    display: "flex",
+    justifyContent: "space-between",
+    alginItems: "center",
+})
+
+export default FlexBetween;
